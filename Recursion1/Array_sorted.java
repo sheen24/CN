053221a7af78b008bfa10d4fad0d52b361e44c90
sheen24 <142446632+sheen24@users.noreply.github.com>
2023-12-26 18:14:51 +0530

    Create Array_sorted.java

---
+++ Recursion1/Array_sorted.java
@@ -0,0 +1,16 @@
+ public class Recursion{
+public static boolean isSorted(int a[]){
+        if(a.length==1){
+            return true;
+        }
+        if(a[0]>a[1]){
+            return false;
+        }
+        int smallArray[]=new int[a.length-1];
+        for(int i=1;i<a.length;i++){
+            smallArray[i-1]=a[i];
+        }
+        boolean isSmallArraySorted=isSorted(smallArray);
+        return isSmallArraySorted;
+    }
+}
