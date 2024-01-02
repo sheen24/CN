23bbdf5911b3f53f68da4b9806d9ce4beba6d58f
sheen24 <142446632+sheen24@users.noreply.github.com>
2024-01-03 00:49:42 +0530

    Create MergeSort.java

---
+++ Recursion2/MergeSort.java
@@ -0,0 +1,101 @@
+Problem statement
+You are given the starting 'l' and the ending 'r' positions of the array 'ARR'.
+
+
+
+You must sort the elements between 'l' and 'r'.
+
+
+
+Note:
+Change in the input array itself. So no need to return or print anything.
+Example:
+Input: ‘N’ = 7,
+'ARR' = [2, 13, 4, 1, 3, 6, 28]
+
+Output: [1 2 3 4 6 13 28]
+
+Explanation: After applying 'merge sort' on the input array, the output is [1 2 3 4 6 13 28].
+Detailed explanation ( Input/output format, Notes, Images )
+Input format :
+The first line contains an integer <em>**'N'**</em> representing the size of the array/list.
+
+The second line contains 'N' single space-separated integers representing the elements in the array/list.
+Output format :
+You don't need to return anything. In the output, you will see the array after you do the modification.
+Sample Input 1:
+7
+2 13 4 1 3 6 28
+Sample Output 1:
+1 2 3 4 6 13 28
+Explanation of Sample Output 1:
+After applying 'merge sort' on the input array, the output is [1 2 3 4 6 13 28].
+Sample Input 2:
+5
+9 3 6 2 0
+Sample Output 2:
+0 2 3 6 9
+Explanation of Sample Output 2:
+After applying 'merge sort' on the input array, the output is [0 2 3 6 9].
+Constraints :
+1 <= N <= 10^3
+0 <= ARR[i] <= 10^9
+// public class Solution {
+
+//     public static void mergeSort(int[] arr, int l, int r){
+//         // Write your code here
+
+//     }
+// }
+
+public class Solution {
+    public static void mergeSort(int[] arr, int l, int r){
+        if (l < r) {
+            int mid = (l + r) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+    
+    public static void merge(int[] arr, int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+        
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+        
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = arr[l + i];
+        }
+        
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+        
+        int i = 0, j = 0, k = l;
+        
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+        
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+}
