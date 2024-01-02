2941cd8e2637638204ff1e4dafd262a47bd6fd4e
sheen24 <142446632+sheen24@users.noreply.github.com>
2024-01-03 01:46:03 +0530

    Create ChainedGenericPairs.java

---
+++ OOPS/ChainedGenericPairs.java
@@ -0,0 +1,61 @@
+package generics;
+
+public class Pair<T,S>{
+    private T first;
+    private S second;
+
+    public Pair(){}
+
+    public Pair(T first, T second){
+      this.first = first;
+      this.second = second;
+    }
+
+    public T getFirst(){
+        return first;
+    }
+
+    public void setFirst(T first){
+        this.first=first;
+    }
+
+    public T getSecond(){
+      return second;
+    }
+
+    public void setSecond(T second){
+      this.second= second;
+    }
+}
+package generics;
+
+public class PairUse {
+
+    public static void main(String[] args){
+
+          Pair<String, Integer> pInner= new Pair<String, Integer>("ab",100);
+          Pair<Pair<String, Integer>, String> p= new Pair<>();
+          p.setFirst(PInner);
+          System.out.println(p.getFirst());
+          System.out.println(p.getFirst().getFirst());
+          System.out.println(p.getFirst().getSecond());
+
+//          Pair p= new Pair(10,20);
+//        System.out.println(p.getFirst()+ " " +p.getSecond());
+
+//        PairString pS= new PairString("aa", "bb");
+//        PairDouble pS= new PairDouble(3.1, 7.2);
+
+//          Pair<String> pS= new Pair<String>("aa", "bb");
+//          System.out.println(pS.getFirst()+" "+ ps.getSecond());
+//          pS.setFirst("xyz");
+
+  //        Pair<Integer> pI= new Pair<Integer>(10, 20);
+  //        System.out.println(pI.getFirst()+ " " +pI.getSecond());
+
+    ///      Pair<Double> pD= new Pair<Double>(10.1, 20.6);
+    //      System.out.println(pD.getFirst()+ " " +pD.getSecond());
+//
+
+    }
+}
