1fd7e2c77827da18c37cbf615d410666284d8f94
sheen24 <142446632+sheen24@users.noreply.github.com>
2024-01-03 00:56:40 +0530

    Create ReturnKeypad(String).java

---
+++ Recursion2/ReturnKeypad(String).java
@@ -0,0 +1,39 @@
+Problem statement
+Given an integer n, using phone keypad find out all the possible strings that can be made using digits of input n.
+
+Return empty string for numbers 0 and 1.
+
+Note :
+1. The order of strings are not important.
+2. Input and output has already been managed for you. You just have to populate the output array and return the count of elements populated in the output array.
+Detailed explanation ( Input/output format, Notes, Images )
+Input Format :
+Integer n
+Output Format :
+All possible strings in different lines
+Constraints :
+1 <= n <= 10^6
+
+Sample Input:
+23
+Sample Output:
+ad
+ae
+af
+bd
+be
+bf
+cd
+ce
+cf
+public class solution {
+
+	// Return a string array that contains all the possible strings
+	public static String[] keypad(int n){
+		// Write your code here
+
+        if(n==0||n==1)
+    {
+        String ans[]={""};
+        return ans;
+
