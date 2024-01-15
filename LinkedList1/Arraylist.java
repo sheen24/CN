09d18bdc3c5a870c27f4f89165e74fdd1de28491
sheen24 <142446632+sheen24@users.noreply.github.com>
2024-01-16 01:41:42 +0530

    Update Arraylist.java

--- LinkedList1/Arraylist.java
+++ LinkedList1/Arraylist.java
@@ -6,13 +6,26 @@ public class ArrayListDemo{
 
     public static void main(String[] args){
         ArrayList<Integer> arr= new ArrayList<>(20);
+        //  ArrayList<Integer> arr= new ArrayList<>();
         arr.add(10);
         arr.add(20);
         arr.add(30);
         arr.add(1,80)
+        arr.remove(20);
+    //    arr.set(0,100)// replaces value
+        Integer i=10;
+        arr.remove(i);
 
-        System.out.println(arr.size());
-        System.out.println(arr.get(1));
+         for(int i=0;i<arr.size();i++){
+            System.out.println(arr.get(i));
+        }
+        // Enhanced for loop - for each loop
+        for(int i:arr){
+          System.out.println(i);
+        }
+
+        //System.out.println(arr.size());
+        //System.out.println(arr.get(1));
 
     }
 }
